b6ffe93c486b1d2ae89a076a86efe8e49973be99
clf0022git <clf0022@uah.edu>
2025-01-18 16:28:55 -0600

    Remove chris text

--- scripts/player.gd
+++ scripts/player.gd
@@ -8,8 +8,6 @@ const JUMP_VELOCITY = -300.0
 
 func _physics_process(delta: float) -> void:
 	
-	print("Hello I'm Chris")
-	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
