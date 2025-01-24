ab06c53062e6e461a33e2907e14f1723e1c08ce8
crazymodemax <apanettieri11@gmail.com>
2025-01-23 20:17:21 -0600

    added bullet reflectors & switches/doors !!

--- scripts/bullet_beta.gd
+++ scripts/bullet_beta.gd
@@ -4,8 +4,8 @@ extends AnimatableBody2D
 var shot_angle
 var spawn_position
 var facing_scale
-
-
+var speed = 200
+var velocity
 @onready var timer: Timer = $Timer
 
 
@@ -13,17 +13,24 @@ var facing_scale
 func _ready() -> void:
 	global_position = spawn_position
 	rotation = shot_angle
-	
+	velocity = Vector2(speed * facing_scale, 0).rotated(shot_angle)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	
-	var collision = move_and_collide( Vector2(200 * facing_scale, 0).rotated(shot_angle) * delta )
-	
+	var collision = move_and_collide( velocity * delta )
 	if collision:
-		queue_free()
-	elif timer.get_time_left() == 0:
+		print(collision.get_collider())
+		if 'reflect' in collision.get_collider().to_string():
+			velocity = velocity.bounce(collision.get_normal())
+		else:
+			queue_free()
+
+	if timer.get_time_left() == 0:
 		queue_free()
 
 func _on_body_entered(body: Node2D) -> void:
-	queue_free()
+	if body.name.match("reflect"):
+		print('hi')
+	else:
+		queue_free()
