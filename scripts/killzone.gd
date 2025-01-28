6f05cfc66437fca87be823000d052da41bb0755d
clf0022git <clf0022@uah.edu>
2025-01-28 17:58:33 -0600

    Imported Luke's Assets and worked a little on my levels

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -6,7 +6,6 @@ func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		print("You died!")
 		$PlayerDeathSound.play()
-		Engine.time_scale = 0.5
 		body.get_node("CollisionShape2D").queue_free()
 		timer.start()
 	else:
@@ -14,5 +13,4 @@ func _on_body_entered(body: Node2D) -> void:
 
 
 func _on_timer_timeout() -> void:
-	Engine.time_scale = 1.0
 	get_tree().reload_current_scene()
